04ca91c110beebb2810de088fa5cd49c3d3d851a
Rafael Dantas <rafael.dantas2001@hotmail.com>
2019-09-27 20:35:48 -0300

    Aplicacao CSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastroClienteComponent } from './cadastro-cliente/cadastro-cliente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroClienteComponent
   ],
   imports: [
     BrowserModule,
