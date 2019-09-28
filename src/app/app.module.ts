eb605cce2ffed3a011b12b97a15f60b2eca57e49
Rafael Dantas <rafael.dantas2001@hotmail.com>
2019-09-27 22:33:20 -0300

    Salvando informacoes do formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CadastroClienteComponent } from './cadastro-cliente/cadastro-cliente.component';
+import {ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CadastroClienteComponent
   ],
+
+
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/cadastro-cliente/cadastro-cliente.component.ts
+++ src/app/cadastro-cliente/cadastro-cliente.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import {FormBuilder , FormGroup} from '@angular/forms'
+
+
+
+
+
 @Component({
   selector: 'has-cadastro-cliente',
   templateUrl: './cadastro-cliente.component.html',
@@ -7,9 +13,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroClienteComponent implements OnInit {
 
-  constructor() { }
+  formCliente : FormGroup;
+
+   constructor(public formBuilder : FormBuilder) { }
 
   ngOnInit() {
+
+    this.formCliente = this.formBuilder.group({
+      nome : this.formBuilder.control(''),
+      cpf : this.formBuilder.control(''),
+      email : this.formBuilder.control(''),
+      telefone : this.formBuilder.control(''),
+      endereco : this.formBuilder.control('')
+
+
+    })
+
+    
+
+  }
+
+  onSubmit () {
+    console.log(this.formCliente.value);
   }
 
 }
